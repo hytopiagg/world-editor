832b744d34ad7c3f7259868d8fb5ef08fc079958
Temptranquil <max@hytopia.com>
2025-04-08 17:04:10 -0700

    Relocated the save tool

--- src/js/components/ToolBar.js
+++ src/js/components/ToolBar.js
@@ -18,7 +18,8 @@ import {
 	FaCubes,
 	FaDatabase,
 	FaBorderAll,
-	FaSeedling
+	FaSeedling,
+	FaSave
 } from 'react-icons/fa';
 import Tooltip from "../components/Tooltip";
 import { DatabaseManager, STORES } from "../DatabaseManager";
@@ -32,7 +33,7 @@ import {
 } from '../ImportExport';
 import { DISABLE_ASSET_PACK_IMPORT_EXPORT } from '../Constants';
 import MinecraftImportWizard from './MinecraftImportWizard';
-const ToolBar = ({ terrainBuilderRef, mode, handleModeChange, axisLockEnabled, setAxisLockEnabled, placementSize, setPlacementSize, setGridSize, undoRedoManager, currentBlockType, environmentBuilderRef }) => {
+const ToolBar = ({ terrainBuilderRef, mode, handleModeChange, axisLockEnabled, setAxisLockEnabled, placementSize, setPlacementSize, setGridSize, undoRedoManager, currentBlockType, environmentBuilderRef, setIsSaving }) => {
 	const [newGridSize, setNewGridSize] = useState(100);
 	const [showDimensionsModal, setShowDimensionsModal] = useState(false);
 	const [dimensions, setDimensions] = useState({
@@ -600,7 +601,7 @@ const ToolBar = ({ terrainBuilderRef, mode, handleModeChange, axisLockEnabled, s
 					<div className="control-label">Shape Tools</div>
 				</div>
 
-				<div className="control-group contol-group-end">
+				<div className="control-group">
 					<div className="control-button-wrapper">
 						<Tooltip text="Change grid size">
 							<button
@@ -635,6 +636,30 @@ const ToolBar = ({ terrainBuilderRef, mode, handleModeChange, axisLockEnabled, s
 						</Tooltip>
 					</div>
 					<div className="control-label">Map Tools</div>
+				</div>
+
+				<div className="control-group contol-group-end">
+					
+					<div className="control-button-wrapper">
+					<Tooltip text="Save terrain (Ctrl+S)">
+						<button
+							onClick={() => {
+							// Use the setIsSaving prop here
+							setIsSaving(true);
+							// Then call the actual save function
+							if (terrainBuilderRef.current) {
+								terrainBuilderRef.current.saveTerrainManually();
+							}
+							// Set a fallback timer to clear the saving state if something goes wrong
+							setTimeout(() => setIsSaving(false), 5000);
+							}}
+							className="control-button"
+						>
+							<FaSave />
+						</button>
+					</Tooltip>
+					</div>
+					<div className="control-label">Save</div>
 				</div>
 			</div>
 
